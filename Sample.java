f3a2ba822adfdb4c1069f9d488b975860089bc6c
Govindasamy Subramaniyan <147674764+govindasamys@users.noreply.github.com>
2023-11-20 13:54:28 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -3,6 +3,7 @@ public class helloworld {
  public static void main(String[] args) {
 	//TODO Auto-generated method stub
 		System.out.println("Hello Samy");
-		System.out.println("Welcome to Jenkins world"); //newly update
+		System.out.println("Welcome to Jenkins world");
+	        System.out.println("Welcome to Jenkins"); //newly update
  }
 }
